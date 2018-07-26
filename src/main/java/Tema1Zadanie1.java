160b5f50508e93292d17ea872d92966ae443995a
skvozhnec <skvozhnec@gmail.com>
2018-07-26 23:21:51 +0300

    тема 1 задание 1

---
+++ src/main/java/Tema1Zadanie1.java
@@ -0,0 +1,20 @@
+public class Tema1Zadanie1 {
+    public static void main(String[] args) {
+        int q = 55;
+        int v = 10;
+        System.out.print(q);
+        System.out.print(" / ");
+        System.out.print(v);
+        System.out.print(" = ");
+        System.out.print(q / v);
+        System.out.print(" и ");
+        System.out.print(q % v);
+        System.out.print(" в остатке");
+
+    }
+}
+//В переменных q и w хранятся два натуральных числа. Создайте программу,
+// выводящую на экран результат деления q на w с остатком.
+// Пример вывода программы (для случая, когда в q хранится 21, а в w хранится 8):
+//
+//21 / 8 = 2 и 5 в остатке
