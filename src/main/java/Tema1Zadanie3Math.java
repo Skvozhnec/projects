726c1e0de0dd25ba1fe627b1bd22ec3f244298e1
skvozhnec <skvozhnec@gmail.com>
2018-07-31 22:09:32 +0300

    тема 1 задание 3 с math

---
+++ src/main/java/Tema1Zadanie3Math.java
@@ -0,0 +1,11 @@
+public class Tema1Zadanie3Math {
+
+
+    /*В переменной n хранится вещественное число с ненулевой дробной частью.
+Создайте программу, округляющую число n до ближайшего целого и выводящую результат на экран.*/
+
+        public static void main(String[] args) {
+            double n = 3.6; //
+            System.out.println(Math.round(n));
+        }
+    }
