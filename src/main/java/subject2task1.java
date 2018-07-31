67169ed32b4d71bb67dc2b883132150ef1841cbf
skvozhnec <skvozhnec@gmail.com>
2018-07-31 22:33:32 +0300

    subject 2 task 1

---
+++ src/main/java/subject2task1.java
@@ -0,0 +1,15 @@
+/*Вычислить и вывести на экран косинусы углов в 60, 45 и 40 градусов
+ без использования функции Math.toDegrees(n).*/
+public class subject2task1 {
+
+    public static void main(String[]args){
+
+    int a = 60;
+    int b = 45;
+    int c = 40;
+
+    System.out.println(Math.cos(a));
+    System.out.println(Math.cos(b));
+    System.out.println(Math.cos(c));
+}
+}
